76fa71bbafa79c7a5f7acf8259474a9cec0f07ae
Mateus Santos <a.mateussm1.5@gmail.com>
2023-03-28 23:16:20 -0300

    Pressing right and left arrows

--- gester/__main__.py
+++ gester/__main__.py
@@ -1,6 +1,7 @@
-import os
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import pyautogui
+import os
 
 # Variables
 width, height = 1280, 720
@@ -17,10 +18,10 @@ path_images = sorted(os.listdir(folder_path))
 
 # Variables
 image_number = 0
-scale = 1
-hs, ws = int(120*scale), int(213*scale)
+scale = .3
 gesture_threshold = 400
 pressed_button = False
+show_cam = False
 
 #  Hand Detector
 detector = HandDetector(
@@ -28,14 +29,12 @@ detector = HandDetector(
     maxHands=1,
 )
 button_counter = 0
-button_delay = 10 
+button_delay = 10
 
 while True:
     # Import images
     sucess, img = cap.read()
     img = cv2.flip(img, 1)
-    full_image_path = os.path.join(folder_path, path_images[image_number])
-    current_image = cv2.imread(full_image_path)
 
     hands, img = detector.findHands(img)
     cv2.line(img, (0, gesture_threshold), (width,  gesture_threshold), (0, 255, 0), 10)
@@ -49,34 +48,32 @@ while True:
         if cy <= gesture_threshold:
             # Gesture 1 - Left
             if fingers == [1, 0, 0, 0, 0]:
-                print('Left')
-                if image_number > 0:
-                    image_number -= image_number
-                    pressed_button = not pressed_button
+                print('left')
+                pyautogui.press('left')
+                pressed_button = True
 
             # Gesture 2 - Right
-            elif fingers == [0, 0, 0, 0, 1]:
+            if fingers == [0, 0, 0, 0, 1]:
                 print('Right')
-                if image_number < len(path_images) - 1:
-                    image_number += image_number
-                    pressed_button = not pressed_button
+                pyautogui.press('right')
+                pressed_button = True
 
+            # Gesture 3 - Quit
+            if fingers == [0, 1, 0, 0, 1]:
+                print('Rock')
+                break
 
     # Button pressed itterations
     if pressed_button:
         button_counter += 1
         if button_counter > button_delay:
             button_counter = 0
-            pressed_button = not pressed_button
+            pressed_button = False
 
-
-    # Addin webcam image on the slides
-    small_image = cv2.resize(img, (ws, hs))
-    h, w, _ = current_image.shape
-    current_image[0:hs, w-ws:w] = small_image
-
-    cv2.imshow('Image', img)
-    cv2.imshow('Slide', current_image)
+    if show_cam:
+        h, w, _ = img.shape
+        image_on_screen = cv2.resize(img, (int(w*scale), int(h*scale)))
+        cv2.imshow('Image', image_on_screen)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
